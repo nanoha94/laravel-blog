67616eb12c049e961beac61bd57f09d35ba14a29
nanoha <nanoha.creator@gmail.com>
2023-08-08 16:42:03 +0100

    ファイル

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -1,12 +1,14 @@
 <?php
 
-namespace App\Models\Models;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Blog extends Model
 {
+    use HasFactory;
+    
     // テーブル名
     protected $table = 'blogs';
 
