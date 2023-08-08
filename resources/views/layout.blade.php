f43d0d60ecb870e2e91d05079273868c0d63f24d
nanoha <nanoha.creator@gmail.com>
2023-08-08 19:34:54 +0100

    ブログ登録機能を

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,6 +2,7 @@
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
+    <meta name="csrf-token" content="{{csrf_token()}}">
     <title>@yield('title')</title>
     <link rel="stylesheet" href="{{ asset('build/assets/app-3ea8b221.css') }}">
     <script src="{{ asset('build/assets/app-d4b42df8.js') }}"></script>
